2aa46536ecbed072f8d38a596eb2c1858d152d4f
DavidMandizera <davidtmandizera@gmail.com>
2022-07-03 19:12:44 -0700

    third attempt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,37 @@
-nclude <stdio.h>
+/*
+ *
+ *  * File: 4-puts.c
+ *
+ *   * Auth: Brennan D Baraban <375@holbertonschool.com>
+ *
+ *    */
+
+
+
+#include <stdio.h>
+
+
 
 /**
  *
- *  * main - main block
+ *  * main - Prints "Programming is like building a multilingual
  *
- *   * Return: 0
+ *   *                puzzle, followed by a new line.
  *
- *    */
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+
+
+			return (0);
 
 }
