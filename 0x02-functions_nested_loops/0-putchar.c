cb31bd3e1625180d6e9f387a85a9f1292ac771a0
DavidMandizera <davidtmandizera@gmail.com>
2022-07-05 13:24:16 -0700

    fasdg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,44 @@
+#include <stdio.h>
+
 #include "main.h"
 
+
+
 /**
- * main - Entry point
  *
- *  Return: nothing, return void
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: A program that prints _putchar and new line
+ *
+ *     *
+ *
+ *      * Return: returns 0
+ *
+ *       **/
 
 int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
 
-	return (0);
+{
+
+		char c[8] = "_putchar";
+
+			int i = 0;
+
+
+
+				while (i < 8)
+
+						{
+
+									_putchar(c[i]);
+
+											i++;
+
+												}
+
+					_putchar('\n');
+
+						return (0);
 }
