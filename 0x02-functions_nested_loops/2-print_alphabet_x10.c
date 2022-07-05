6162cff7381bd69884aa45b0ca5981a088b4b381
DavidMandizera <davidtmandizera@gmail.com>
2022-07-05 15:47:06 -0700

    dhjf

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * print_alphabet - Begin entry
- * Return: 0
- */
+ *  * print_alphabet_x10 - prints alphabet 10 times
+ *   * Return: void
+ *    */
 
-void print_alphabet_x10(void); 
+void print_alphabet_x10(void)
 {
-	char c = 'a';
+		char c;
+			int i = 0;
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-
-	}
-	_putchar('\n');
-
+				while (i < 10)
+						{
+									c = 'a';
+											while (c <= 'z')
+														{
+																		_putchar(c);
+																					c++;
+																							}
+													_putchar('\n');
+															i++;
+																}
 }
