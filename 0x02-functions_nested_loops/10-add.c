a0a2aed3f9f808b7b6abf3b5d218af1216493258
DavidMandizera <davidtmandizera@gmail.com>
2022-07-05 12:15:08 -0700

    firstdx

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,29 @@
+nclude "holberton.h"
+
+
+
+/**
+ *
+ *  * add -  sum between two numbers
+ *
+ *   *
+ *
+ *    * @a : number one
+ *
+ *     *
+ *
+ *      * @b : number two
+ *
+ *       * Return : sum
+ *
+ *        **/
+
+
+
+int add(int a, int b)
+
+{
+
+		return (a + b);
+
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,43 @@
+nclude "holberton.h"
+
+
+
+/**
+ *
+ *  * _islower - Check if the entry is in lower.
+ *
+ *   * @c: This is the entry
+ *
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
+
+
+
+int _islower(int c)
+
+{
+
+		if (c >= 'a' && c <= 'z')
+
+				{
+
+							return (1);
+
+								}
+
+			else
+
+					{
+
+								return (0);
+
+									}
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
